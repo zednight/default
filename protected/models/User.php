4f39f06ba7ec4d47f9c962decb5d49eabc372ea6
zednight <zednight@zedhome>
2012-05-13 20:29:52 +0400

    Авторизация

--- protected/models/User.php
+++ protected/models/User.php
@@ -1,8 +1,17 @@
 <?php
-class User extends PUser
-{
-	
+
+class User extends PUser {
+
+	public function validatePassword($password) {
+		return $this->hashPassword($password, $this->salt) === $this->password;
+	}
+
+	public function hashPassword($password, $salt) {
+		return md5($salt . $password);
+	}
+
 }
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
