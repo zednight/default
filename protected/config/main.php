8e057371740d40694e86f7e302faf08f330e5ca4
zednight <zednight@zedhome>
2012-05-13 19:35:33 +0400

    Подключение базы и форматирование

--- protected/config/main.php
+++ protected/config/main.php
@@ -2,39 +2,34 @@
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
-
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
-
+	'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+	'name' => 'My Web Application',
 	// preloading 'log' component
-	'preload'=>array('log'),
-
+	'preload' => array('log'),
 	// autoloading model and component classes
-	'import'=>array(
+	'import' => array(
 		'application.models.*',
 		'application.components.*',
 	),
-
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
+	'modules' => array(
+	// uncomment the following to enable the Gii tool
+	/*
 		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
-		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+		'class'=>'system.gii.GiiModule',
+		'password'=>'Enter Your Password Here',
+		// If removed, Gii defaults to localhost only. Edit carefully to taste.
+		'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+	*/
 	),
-
 	// application components
-	'components'=>array(
-		'user'=>array(
+	'components' => array(
+		'user' => array(
 			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
+			'allowAutoLogin' => true,
 		),
 		// uncomment the following to enable URLs in path-format
 		/*
@@ -46,45 +41,43 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-		'db'=>array(
+		 */
+		/* 'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+			), */
 		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+
+		'db' => array(
+			'connectionString' => 'mysql:host=localhost;dbname=mybase',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'bchfsl2',
 			'charset' => 'utf8',
 		),
-		*/
-		'errorHandler'=>array(
+		'errorHandler' => array(
 			// use 'site/error' action to display errors
-            'errorAction'=>'site/error',
-        ),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
+			'errorAction' => 'site/error',
+		),
+		'log' => array(
+			'class' => 'CLogRouter',
+			'routes' => array(
 				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'class' => 'CFileLogRoute',
+					'levels' => 'error, warning',
 				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
+			// uncomment the following to show log messages on web pages
+			/*
+			array(
+				'class'=>'CWebLogRoute',
+			),
+			 */
 			),
 		),
 	),
-
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params'=>array(
+	'params' => array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail' => 'webmaster@example.com',
 	),
 );
